5b7c6b99a3880825636bbfcab56aa26747f89813
Casey Manning <kcgrind@gmail.com>
2019-02-13 15:48:12 -0800

    refactored & redid perspective transform

--- test2.py
+++ test2.py
@@ -2,15 +2,17 @@ import cv2
 import numpy as np
 import transform
 from transform import transformImage
+from transform import warp_perspective_new
 import sys
 import time
+import imutils
 
 live = False;
 robotImage = cv2.imread('robot.png')
-robotHeight, robotWidth, channels = robotImage.shape
+# robotHeight, robotWidth, channels = robotImage.shape
 n = 0
 
-cameras = [0, 1, 2, 3, 4]
+cameras = [0, 1, 2]
 
 if live:
 	cameras = [cv2.VideoCapture(x) for x in cameras]
@@ -21,39 +23,48 @@ maxHeight = 1000
 
 robotImage = cv2.imread('robot.png')
 
-#350, 344), (609, 336), (960, 503), (0, 503)
-p1 = (290, 335)
-p2 = (670, 335)
-p3 = (0, 540)
-p4 = (959, 540)
+origFisheye = cv2.imread('fisheye.png')
 
 DIM=(960, 540)
+DIM2=(960, 540)
 K=np.array([[263.1021173128426, 0.0, 477.98780306608234], [0.0, 261.30612719984185, 300.714230825097], [0.0, 0.0, 1.0]])
 D=np.array([[-0.0007727739728155351], [-0.10019345132548932], [0.10790597488851726], [-0.040655761660861996]])
 
 lastTime = time.time()
 
-images = [cv2.imread('0testFisheye.png'), cv2.imread('1testFisheye.png'), cv2.imread('2testFisheye.png')]
+images = [cv2.imread('pinCenter.png'), cv2.imread('pinLeft.png'), cv2.imread('pinRight.png')]
+
+p1 = (401, 400)
+p2 = (586, 400)
+p3 = (0, 540)
+p4 = (960, 540)
+uah =  np.array([p1, p2, p3, p4], dtype="float32")
+
 
 def getImage(cam):
 	retval, image = cam.read()
 	return image
-
-def putTogetherImages(i1, i2, i3, i4):
-	p1 = np.concatenate((i1, robotImage), axis=0)
-	return np.concatenate((p1, i2), axis=0)
 
 def undistort(img):
 	h,w = img.shape[:2]
 	map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
 	return cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
-def putTogetherImages(i1, i2, i3, i4):
-	p1 = np.concatenate((i1, robotImage), axis=0)
-	return np.concatenate((p1, i2), axis=0)
+def putTogetherImages(images):
+	print("images: ", len(images))
+	cameraPositions = [(1500, 1000), (1500, 2000), (1000, 1500), (2000, 1500)]
+	rotated_images = images[:]
+	for i, image in enumerate(images):
+		rotated_images[i] = imutils.rotate_bound(image, i*90)
+	composite_image = np.zeros((3000,3000,3), np.uint8)
 
-while True:
-	if live:
+	for pos, image in zip(cameraPositions, rotated_images):
+		composite_image[pos[1]:pos[1]+image.shape[0], pos[0]:pos[0]+image.shape[1]] = image
+
+	return composite_image
+
+if live:
+	while True:
 		images = [getImage(x) for x in cameras]
 		nulls = []
 		for i in range(len(images)):
@@ -64,45 +75,67 @@ while True:
 		nulls.reverse()
 		for i in nulls:
 			images.pop(i)
-	if True:
-		try:
-			images = [cv2.resize(x, (960, 540)) for x in images]
-		except:
-			print('Null Image')
-			continue
-
-		ct = time.time()
-		timePassed = ct - lastTime
-		lastTime = ct
-		print('FPS: ' + str(1/timePassed))
-
+		images = [cv2.resize(x, (960, 540)) for x in images]
 		undistorted = [undistort(x) for x in images]
+		transformed = [transformImage(x, uah) for x in undistorted]
+		for im in undistorted:
+			cv2.circle(im, p1, 2, (0,255,0), thickness=3, lineType=8, shift=0)
+			cv2.circle(im, p2, 2, (0,255,0), thickness=3, lineType=8, shift=0)
+			cv2.circle(im, p3, 2, (0,255,0), thickness=3, lineType=8, shift=0)
+			cv2.circle(im, p4, 2, (0,255,0), thickness=3, lineType=8, shift=0)
 
-		# calibrationImage = cv2.resize(image, (960, 540))
-		# cv2.imshow("calibration", calibrationImage);
-		# calibrationPoints = transform.getTransformPoints(calibrationImage, (9, 6))
-		# print(calibrationPoints)
-		for img in undistorted:
-			cv2.circle(img, p1, 2, (0,255,0), thickness=3, lineType=8, shift=0)
-			cv2.circle(img, p2, 2, (0,255,0), thickness=3, lineType=8, shift=0)
-			cv2.circle(img, p3, 2, (0,255,0), thickness=3, lineType=8, shift=0)
-			cv2.circle(img, p4, 2, (0,255,0), thickness=3, lineType=8, shift=0)
-
-		for i in range(len(images)):
-			cv2.imshow('Original1' + str(i), images[i])
-
-		warped = [cv2.flip(transformImage(x), 1) for x in undistorted]
-
-		for i in range(len(warped)):
-			cv2.imshow('undistorted' + str(i), undistorted[i])
-
-		topDown = putTogetherImages(warped[0], warped[1], warped[2], robotImage)
-		cv2.imshow('allTogether' + str(i), undistorted[i])
+		for i in range(len(transformed)):
+			cv2.imshow('undistorted' + str(i), images[i])
+			cv2.imshow('undistored' + str(i), undistorted[i])
+			cv2.imshow('transformed' + str(i), transformed[i])
 
 		key = cv2.waitKey(1)
 		if key == 27:
 			sys.exit()
-		if key == 32:
-			print('saving images')
-			for i in range(len(images)):
-				cv2.imwrite(str(i) + 'testFisheye.png', images[i])
+
+try:
+	images = [cv2.resize(x, (960, 540)) for x in images]
+except:
+	print('Null Image')
+
+# ct = time.time()
+# timePassed = ct - lastTime
+# lastTime = ct
+# print('FPS: ' + str(1/timePassed))
+
+# cv2.imshow('OriginalA', images[2])
+# tri1 = np.float32([[[0,450], [620,337], [620,450]]])
+# tri2 = np.float32([[[0,450], [620,337], [620,450]]])
+#
+# r1 = cv2.boundingRect(tri1)
+# r2 = cv2.boundingRect(tri2)
+
+# warpMat = cv2.getAffineTransform( np.float32(tri1), np.float32(tri2))
+# images[2] = cv2.warpAffine(images[2], warpMat, (r2[2], r2[3]), None, flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT_101)
+# for i in range(len(images)):
+# 	cv2.imshow('Original1' + str(i), images[i])
+
+undistorted = undistort(origFisheye)
+
+warped = warp_perspective_new(uah, undistorted)
+
+cv2.circle(undistorted, p1, 2, (255,255,0), thickness=3, lineType=8, shift=0)
+cv2.circle(undistorted, p2, 2, (0,0,255), thickness=3, lineType=8, shift=0)
+cv2.circle(undistorted, p3, 2, (255,0,0), thickness=3, lineType=8, shift=0)
+cv2.circle(undistorted, p4, 2, (0,255,0), thickness=3, lineType=8, shift=0)
+
+
+cv2.imshow('1riginal', origFisheye)
+cv2.imshow('2riginal', undistorted)
+cv2.imshow('3riginal', warped)
+# cv2.imshow('Top Down 2', warpedBad)
+
+
+while True:
+	key = cv2.waitKey(10000)
+	if key == 27:
+		sys.exit()
+	if key == 32:
+		print('saving images')
+		for i in range(len(images)):
+			cv2.imwrite(str(i) + 'testFisheye.png', images[i])
--- transform.py
+++ transform.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2
 
-topDownRes = (960, 540)
+topDownRes = (600, 600)
 
-p1 = (290, 335)
-p2 = (670, 335)
-p3 = (0, 540)
-p4 = (960, 540)
-pts = np.array((p2,p1,p3,p4),dtype=np.float32)
+p1 = (478, 600)
+p2 = (800, 600)
+p3 = (0, 391)
+p4 = (1260, 391)
+pts2 = np.array((p2,p1,p3,p4),dtype=np.float32)
 
 def warp_perspective(rect, grid):
 	(tl, tr, br, bl) = rect
@@ -18,13 +18,20 @@ def warp_perspective(rect, grid):
 		[topDownRes[0] - 1, topDownRes[1] - 1],
 		[0, topDownRes[1] - 1]], dtype="float32")
 
-	# the perspective to grab the screen
 	M = cv2.getPerspectiveTransform(rect, dst)
 	warp = cv2.warpPerspective(grid, M, (topDownRes[0], topDownRes[1]))
 	return warp
 
-def transformImage(image, points=pts):
-	return warp_perspective(pts, image)
+def warp_perspective_new(rect, image):
+
+	dst = np.array([(300,300), (700, 300), (300, 700), (700, 700)], dtype="float32")
+	h, mask = cv2.findHomography(rect, dst)
+	warpedImage = cv2.warpPerspective(image, h, (1000, 1000))
+	return warpedImage
+
+
+def transformImage(image, points=pts2):
+	return warp_perspective(points, image)
 
 def getTransformPoints(callibration_image, checkerboard_dims):
 	return cv2.findChessboardCorners(callibration_image, checkerboard_dims, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
